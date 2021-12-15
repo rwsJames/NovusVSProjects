a9c69d89692e202cad9227e21a646bc27da281e4
Kwhale <ryan.wsjames@gmail.com>
2021-12-15 12:32:55 +0000

    CRUD Worksheet Projects

---
+++ CRUDWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,49 @@
+﻿using CRUDLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace CRUDWebAPI.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        CollegeEntities OE = new CollegeEntities();
+
+        // api/Students/
+        public IQueryable<Student> Get()
+        {
+            return OE.Students;
+        }
+
+        // api/Students/1
+        public Student Get(int id)
+        {
+            Student student = OE.Students.Find(id);
+            return student;
+        }
+
+        // ../api/department
+        public void Put(int id, Student student)
+        {
+            OE.Entry(student).State = System.Data.Entity.EntityState.Modified;
+            OE.SaveChanges();
+        }
+
+        // ../api/department
+        public void Delete(int id)
+        {
+            Student student = OE.Students.Find(id);
+            OE.Students.Remove(student);
+            OE.SaveChanges();
+        }
+
+        // ..api/Students/
+        public void Post(Student student)
+        {
+            OE.Students.Add(student);
+            OE.SaveChanges();
+        }
+    }
+}
