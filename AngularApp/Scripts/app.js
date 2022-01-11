45d260cfc5734a1e643b4d0d7fc329482e6fa84a
Kwhale <ryan.wsjames@gmail.com>
2022-01-11 13:27:44 +0000

    Angular examples

---
+++ AngularApp/Scripts/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("Ex2App", []);
+app.controller("Ex2Controller", function ($scope) {
+    $scope.Ex2Val = "Welcome to Angular!";
+
+    $scope.Ex2Func = function () {
+        alert("Hello, friend.");
+    }
+    $scope.Ex2FuncParam = function (val) {
+        alert("Param passed: " + val);
+    }
+});
